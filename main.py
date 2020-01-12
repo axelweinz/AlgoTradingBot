5a39d8cb719a8912f832439d4420913bb233456f
Sayaja <35453348+Sayaja@users.noreply.github.com>
2020-01-12 22:14:02 +0100

    Scraping stock symbols from eoddata is working

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import alpaca_trade_api as alpaca
 import yfinance as yfinance
 import pandas as pandas
 import numpy as numpy
+from bs4 import BeautifulSoup
+import string
+import requests
 from alpacaKeys import alpaca_key_id, alpaca_secret_key
 from tradingLogic import tradingLogic
 
@@ -67,10 +70,36 @@ def scanPortfolio():
 
     return
 
+def findStocks():
+    # Scrape eoddata.com for all stock symbols in NYSE
+    # Retrieve data for all stocks through yfinance
+    # Analyze the stock data to decide which stocks to buy
+
+    alphabet = list(string.ascii_uppercase)
+
+    symbols = []
+
+    # Loop through the letters in the alphabet to get the stocks on each page
+    # from the table and store them in a list
+    for each in alphabet:
+        url = 'http://eoddata.com/stocklist/NYSE/{}.htm'.format(each)
+        response = requests.get(url)
+        site = response.content
+        soup = BeautifulSoup(site, 'html.parser')
+        table = soup.find('table', {'class': 'quotes'})
+        for row in table.findAll('tr')[1:]:
+            symbols.append(row.findAll('td')[0].text.rstrip())
+
+    # Now retrieve data for all symbols through yfinance...
+
+
+    return symbols
+
 #test1 = [44.34, 44.09, 44.15, 43.61, 44.33, 44.83, 45.10, 45.42, 45.84, 46.08, 45.89, 46.03, 45.61, 46.28, 46.28, 46.00, 46.03,
     #46.41, 46.22, 45.64, 46.21, 46.25, 45.71, 46.45, 45.78, 45.35, 44.03, 44.18, 44.22, 44.57, 43.42, 42.66]
 #test3 = [10, 10.15, 10.17, 10.13, 10.11, 10.15, 10.20, 10.20, 10.22, 10.21]
 #test4 = [25200, 30000, 25600, 32000, 23000, 40000, 36000, 20500, 23000, 27500]
 #history = yfinance.download("GOOGL", start="2020-01-01", end="2020-01-07")
 #print(tradingLogic(history, "GOOGL"))
-scanPortfolio()
+symbols = findStocks()
+print(symbols[-1])
